94fb0ce8ba6c5004c895cc106e45a48086ec2012
Nikita <sheihnt@gmail.com>
2024-10-13 20:08:18 +0300

    Add files via upload

---
+++ task1/build/CMakeFiles/MaxValueFinder.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Екатерина/Documents/Nikitosik/aaa/src/fMax.c" "CMakeFiles/MaxValueFinder.dir/src/fMax.c.obj" "gcc" "CMakeFiles/MaxValueFinder.dir/src/fMax.c.obj.d"
+  "C:/Users/Екатерина/Documents/Nikitosik/aaa/src/main.c" "CMakeFiles/MaxValueFinder.dir/src/main.c.obj" "gcc" "CMakeFiles/MaxValueFinder.dir/src/main.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ task1/build/CMakeFiles/MaxValueFinder.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MaxValueFinder.dir/src/fMax.c.obj"
+  "CMakeFiles/MaxValueFinder.dir/src/fMax.c.obj.d"
+  "CMakeFiles/MaxValueFinder.dir/src/main.c.obj"
+  "CMakeFiles/MaxValueFinder.dir/src/main.c.obj.d"
+  "MaxValueFinder.exe"
+  "MaxValueFinder.exe.manifest"
+  "MaxValueFinder.pdb"
+  "libMaxValueFinder.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/MaxValueFinder.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
