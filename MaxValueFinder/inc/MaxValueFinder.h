58495f810c645ae7f59837f78ff1133f89b5c97d
Nikita <sheihnt@gmail.com>
2024-11-05 09:03:00 +0300

    Update MaxValueFinder.h

--- MaxValueFinder/inc/MaxValueFinder.h
+++ MaxValueFinder/inc/MaxValueFinder.h
@@ -1,9 +1,26 @@
-#ifndef MAX_VALUE_FINDER_H
-#define MAX_VALUE_FINDER_H
+#ifndef MAXFINDERVALUE_H
+#define MAXFINDERVALUE_H
 
 #include <stdio.h>
 #include <stdbool.h>
 
-double MaxValueFinder(FILE *file, bool *errorflag_syntax, bool *errorflag_empty);
+/**
+ * @brief Finds the maximum floating-point value in a file.
+ * 
+ * This function reads from a file containing floating-point numbers and determines 
+ * the maximum value. It also checks for syntax errors and empty file conditions 
+ * by setting the provided flags.
+ * 
+ * @param file Pointer to an open file stream for reading.
+ * @param errorflag_syntax Pointer to a boolean flag. Set to `true` if a syntax error 
+ * (non-numeric data) is found in the file.
+ * @param errorflag_empty Pointer to a boolean flag. Set to `true` if the file is empty
+ * or contains no valid numbers.
+ * 
+ * @return The maximum floating-point value found in the file. If the file is empty 
+ * or contains no valid numbers, returns the lowest possible double value, and 
+ * `errorflag_empty` is set to `true`.
+ */
+double fMax(FILE *file, bool *errorflag_syntax, bool *errorflag_empty);
 
-#endif // MAX_VALUE_FINDER_H
+#endif // MAXFINDERVALUE_H
